ba1efa94796bce96768bcb35f2d054583d0771ff
Yasir Eymen Kayabaşı <93262823+yaso09@users.noreply.github.com>
2024-03-23 21:01:54 +0300

    inital commit

---
+++ packages/güncelle.py
@@ -0,0 +1,20 @@
+# Çakıyı güncelleme paketi
+
+import os
+from requests import get as g
+
+build = g("https://raw.githubusercontent.com/yaso09/vld/main/build.sh")
+
+vld = g("https://raw.githubusercontent.con/yaso09/vls/main/vld.py")
+
+f = open("vld.py", "w")
+
+f.write(vld)
+
+f.close()
+
+os.system(build)
+
+os.remove("vld.py")
+
+
--- vld.py
+++ vld.py
@@ -11,7 +11,7 @@ param = sys.argv[1]
 
 if not param: os._exit(0)
 
-if param == "kur" or "-k":
+if param == "çalıştır" or param == "-ç":
     try:
         res = requests.get(
             "https://raw.githubusercontent.com/yaso09/vld/main/packages/{}.py".format(sys.argv[2])
@@ -23,12 +23,12 @@ if param == "kur" or "-k":
     except IndexError: print("""
     Lütfen paket adı girin
     """)
-elif param == "yardım" or "-y":
+elif param == "yardım" or param == "-y":
     print("""
     KOMUT\tKULLANIM\tÖRNEK
 
-    kur     (-k)\tPaketi kur\tvld -k disp
-    yardım  (-y)\tYardım al\tvld -y
+    çalıştır    (-ç)\tPaketi kur\tvld -k disp
+    yardım      (-y)\tYardım al\tvld -y\n
 
     Github deposu: https://github.com/yaso09/vld
     """)
