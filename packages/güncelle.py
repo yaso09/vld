31cfcf7441054745b59a7eb91987a6827d2a542c
Yasir Eymen Kayabaşı <93262823+yaso09@users.noreply.github.com>
2024-03-23 21:09:57 +0300

    yeter la çalış

--- packages/güncelle.py
+++ packages/güncelle.py
@@ -5,7 +5,7 @@ from requests import get as g
 
 build = g("https://raw.githubusercontent.com/yaso09/vld/main/build.sh")
 
-vld = g("https://raw.githubusercontent.con/yaso09/vld/main/vld.py")
+vld = g("https://raw.githubusercontent.com/yaso09/vld/main/vld.py")
 
 f = open("vld.py", "w")
 
